ee8018f2a0f7f8bdb195429cec367d2223c4cf3d
zielaskowski <michal.zielaskowski@gmail.com>
2022-04-05 22:19:15 +0200

    fix CRAN check error

--- R/TTformat.r
+++ R/TTformat.r
@@ -10,7 +10,7 @@
 formatCurrency <- function (table, columns, currency = "$", interval = 3, mark = ",",
                             digits = 2, dec.mark = getOption("OutDec"), before = TRUE)
                   {
-  columns <- incremenCol(columns)
+  columns <- incrementCol(columns)
   DT::formatCurrency(table, columns, currency, interval,
                       mark, digits, dec.mark, before)
 }
@@ -18,7 +18,7 @@ formatCurrency <- function (table, columns, currency = "$", interval = 3, mark =
 #' @rdname formatCurrency
 #' @export
 formatDate <- function (table, columns, method = "toDateString", params = NULL) {
-  columns <- incremenCol(columns)
+  columns <- incrementCol(columns)
   DT::formatDate(table, columns, method , params)
 }
 
@@ -26,7 +26,7 @@ formatDate <- function (table, columns, method = "toDateString", params = NULL) 
 #' @export
 formatPercentage <- function (table, columns, digits = 0, interval = 3, mark = ",",
                               dec.mark = getOption("OutDec")) {
-  columns <- incremenCol(columns)
+  columns <- incrementCol(columns)
   DT::formatPercentage(table, columns, digits, interval, mark, dec.mark)
 }
 
@@ -34,7 +34,7 @@ formatPercentage <- function (table, columns, digits = 0, interval = 3, mark = "
 #' @export
 formatRound <- function(table, columns, digits = 2, interval = 3, mark = ",",
                          dec.mark = getOption("OutDec")){
-  columns <- incremenCol(columns)
+  columns <- incrementCol(columns)
   DT::formatRound(table, columns, digits, interval, mark, dec.mark)
 }
 
@@ -42,14 +42,14 @@ formatRound <- function(table, columns, digits = 2, interval = 3, mark = ",",
 #' @export
 formatSignif <- function(table, columns, digits = 2, interval = 3, mark = ",",
                          dec.mark = getOption("OutDec")){
-  columns <- incremenCol(columns)
+  columns <- incrementCol(columns)
   DT::formatSignif(table, columns, digits, interval, mark, dec.mark)
 }
 
 #' @rdname formatCurrency
 #' @export
 formatString <- function(table, columns, prefix = "", suffix = ""){
-  columns <- incremenCol(columns)
+  columns <- incrementCol(columns)
   DT::formatString(table, columns, prefix, suffix)
 }
 
@@ -58,13 +58,13 @@ formatString <- function(table, columns, prefix = "", suffix = ""){
 formatStyle <- function(table, columns, valueColumns = columns, target = c("cell", "row"),
                         fontWeight = NULL, color = NULL, backgroundColor = NULL,
                         background = NULL, ...){
-  columns <- incremenCol(columns)
-  valueColumns <- incremenCol(valueColumns)
+  columns <- incrementCol(columns)
+  valueColumns <- incrementCol(valueColumns)
   DT::formatStyle(table, columns, valueColumns, target, fontWeight, color, backgroundColor, background, ...)
 }
 
 #' @noRd
-incremenCol <- function(cols){
-  if (class(cols) == "numeric") return( cols + 3)
+incrementCol <- function(cols){
+  if (inherits(cols, "numeric")) return( cols + 3)
   return (cols)
 }
